099326601b34a93ae4904e1795f16b5d10fc2d7e
Le Van Thai <lethai281005@gmail.com>
2026-08-31 23:00:47 +0700

    [CHORE] Refactor the folder structure

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller.js';
 import { UsersService } from './users.service.js';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entity/user.entity.js';
+import { User } from './entities/user.entity.js';
 import { UserRepository } from './repository/user.repository.js';
 import { PassportModule } from '@nestjs/passport';
 
